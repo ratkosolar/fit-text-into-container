e04c0bc8dd25d4871934fc93b6ee34c7db50a9ee
ratkosolar <ratko.solar@gmail.com>
2018-10-24 20:32:19 +0200

    Refactored code

--- src/app/fit-text/fit-text.directive.ts
+++ src/app/fit-text/fit-text.directive.ts
@@ -1,27 +1,11 @@
 import { Directive, Renderer2, ElementRef, OnInit, AfterContentChecked, AfterViewChecked } from '@angular/core';
 
-export interface FTContainer{
-  element: HTMLElement,
-  width?: number,
-  height?: number
-}
-
-export interface FTText{
-  element: HTMLElement,
-  width?: number,
-  height?: number,
-  fontSize?: number
-  fontSizeIncrement?: number
-}
-
 @Directive({
   selector: '[ftFitText]'
 })
 export class FitTextDirective implements OnInit, AfterViewChecked {
 
   element: HTMLElement;
-  container: FTContainer;
-  text: FTText;
 
   constructor(
     private elementRef: ElementRef,
@@ -37,134 +21,74 @@ export class FitTextDirective implements OnInit, AfterViewChecked {
     this.fitText(this.element);
   }
 
-  fitText(containerSelector: string | HTMLElement, textElement?: HTMLElement, prevFontSizeIncrement?: number): boolean{
-    this.container = {element: null};
-    this.text = {element: null};
-    let isRecursive = false;
+  /**
+   * Fit text into container with maximum font size
+   */
+  fitText(containerSelector: string | HTMLElement){
+    let container: HTMLElement;
+    let text: HTMLElement
 
-    // Initialize fitText method params
-    let initialized = this.initParams(containerSelector, textElement);
-    if(!initialized) 
-      return false;
-
-    // Calculate current styles for container and text
-    this.calculateStyles();
-
-    // Check if text is empty
-    if(!this.text.width) 
-      return false;
-
-    // Font size increment (+1, -1 or 0)
-    if(prevFontSizeIncrement){
-      isRecursive = true;
-      this.text.fontSizeIncrement = prevFontSizeIncrement;
-    }else{
-      this.calculateFontSizeIncrement();
-      if(this.text.fontSizeIncrement == 0) 
-        return true;
-    }
-
-    // Check if ideal font size is reached
-    if(isRecursive && this.idealFontSizeReached()) 
-      return true;
-    
-    // Apply new font size
-    this.renderer.setStyle(
-      this.text.element, 
-      'font-size', 
-      this.text.fontSize + this.text.fontSizeIncrement + 'px'
-    );
-    
-    // Recursive call until ideal font size reached
-    return this.fitText(
-      this.container.element, 
-      this.text.element, 
-      this.text.fontSizeIncrement
-    );
-  }
-
-  private initParams(containerSelector: string | HTMLElement, textElement?: HTMLElement): boolean{
-    // Check if HTMLElement is passed directly
+    // Initialize container element
     if(containerSelector instanceof HTMLElement){
-      this.container.element = containerSelector;
+      container = containerSelector;
     }else{
-      this.container.element = document.querySelector(containerSelector);
+      container = document.querySelector(containerSelector);
+      if(!container) return; // no container element found
     }
 
-    // Validate if container element exists
-    if(!this.container.element){
-      console.log('Element %s not found in DOM', containerSelector);
-      return false;
-    }
+    // Find text element
+    text = container.querySelector('span');
+    if(!text) return; // no text element found
 
-    // Check if textElement is passed
-    if(textElement){
-      this.text.element = textElement;
-    }else{
-      this.text.element = this.container.element.querySelector('span');
-    }
-    
-    // Validate if text element exists
-    if(!this.text.element){
-      console.warn('Text element found in DOM');
-      return false;
-    }
-
-    return true;
+    // Calculate and apply max font size
+    let maxFontSize = this.calculateMaxFontSize(container, text);
+    this.updateElementFontSize(text, maxFontSize);
   }
 
-  private calculateStyles(){
-    this.container.width = this.container.element.clientWidth;
-    this.container.height = this.container.element.clientHeight;
-    this.text.width = this.text.element.offsetWidth;
-    this.text.height = this.text.element.offsetHeight;
-    this.text.fontSize = parseInt(window.getComputedStyle(this.text.element).getPropertyValue('font-size').replace('px', ''));
-  }
+  /**
+   * Calculate maximum font size for text inside container 
+   */
+  private calculateMaxFontSize(container: HTMLElement, textElement: HTMLElement): number{
+    // Container dimensions
+    let containerWidth = container.clientWidth;
+    let containerHeight = container.clientHeight;
 
-  private calculateFontSizeIncrement(){
-    if(this.text.width > this.container.width || this.text.height > this.container.height){
-      // current text is too big, decrease font by 1 
-      this.text.fontSizeIncrement = -1;
-    }else if(this.text.width < this.container.width){
-      // current text is too small, increase font by 1
-      this.text.fontSizeIncrement = 1;
-    }else if(this.text.width == this.container.width || this.text.height == this.container.height){
-      // current text already fits
-      this.text.fontSizeIncrement = 0;
-    }
-  }
+    // Text dimensions
+    let textWidth = textElement.offsetWidth;
+    let textHeight = textElement.offsetHeight;
+    let textFontSize = this.calculateCurrentFontSize(textElement);
 
-  private idealFontSizeReached(): boolean{
-    if(this.text.fontSize <= 1){
-      // Reached too small font
-      return true;
-    } 
-    
-    // Positive increment (trying to increase font until max)
-    if(this.text.fontSizeIncrement > 0){
-      if(this.text.width > this.container.width || this.text.height > this.container.height){
-        // Overreached max font size, decrease by 1
-        this.renderer.setStyle(
-          this.text.element, 
-          'font-size', 
-          this.text.fontSize - this.text.fontSizeIncrement + 'px'
-        );
-        return true;
-      }else if(this.text.width == this.container.width || this.text.height == this.container.height){
-        // Reached fitting size
-        return true;
-      }
-    } 
-    
-    // Negative increment (trying to decrease font until it fits)
-    if(this.text.fontSizeIncrement < 0){ 
-      if(this.text.width <= this.container.width && this.text.height <= this.container.height){
-        // Reached fitting size
-        return true;
-      }
+    // No text
+    if(textWidth === 0){
+      return 0;
     }
 
-    return false;
+    // Calculate text to container dimensions ratio
+    let widthRatio = containerWidth/textWidth;
+    let heightRatio = containerHeight/textHeight;
+
+    let maxFontSize = Math.min(widthRatio, heightRatio)*textFontSize;
+    return Math.floor(maxFontSize);
+  }
+
+  /**
+   * Calculate element current computed font size
+   */
+  private calculateCurrentFontSize(element: HTMLElement): number{
+    let style = window.getComputedStyle(element);
+    let fontSize = style.getPropertyValue('font-size');
+    return parseInt(fontSize);
+  }
+
+  /**
+   * Update element font size
+   */
+  private updateElementFontSize(element: HTMLElement, fontSize?: number){
+    this.renderer.setStyle(
+      element,
+      'font-size',
+      fontSize ? fontSize + 'px' : ''
+    );
   }
 
 }
